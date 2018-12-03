0670b690df3bf64b9aa703d61361ac277f9424d6
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2018-12-03 18:17:17 -0500

    Beautified JavaScript

--- src/script.js
+++ src/script.js
@@ -4,78 +4,77 @@ var nodes = new vis.DataSet();
 var edges = new vis.DataSet();
 
 var data = {
-	nodes: nodes,
-	edges: edges
+      nodes: nodes,
+      edges: edges
 };
 
 const directory = function(input) {
-	var dir = "";
-	input = input.split("\\");
-	for (var i = 0; i < input.length - 1; i++) {
-		dir += input[i];
-		if (i < input.length - 2) {
-			dir += "\\";
-		}
-	}
-	return dir;
+      var dir = "";
+      input = input.split("\\");
+      for (var i = 0; i < input.length - 1; i++) {
+            dir += input[i];
+            if (i < input.length - 2) {
+                  dir += "\\";
+            }
+      }
+      return dir;
 }
 
 var id = 0;
 var group = 1;
-const update = function () {
-	var input = document.querySelector("#input").value;
-	input = input.split("\n");
-	
-	for (var i = 0; i < input.length; i++) {
-		var split = input[i].split("\\");
-		var name = split[split.length - 1];
-		if (name.includes(".")) {
-			group = 1;
-		}
-		else {
-			group = 2;
-		}
-		nodes.add({
-			id: id,
-			label: name,
-			group: group,
-			path: input[i]
-		});
-		
-		id++;
-	}
-	for (var i = 0; i < input.length; i++) {
-		for (var j = 0; j < Object.keys(data.nodes._data).length; j ++) {
-			if (directory(input[i]) == data.nodes._data[j].path) {
-				edges.add({
-					from: j,
-					to: i
-				});
-				//nodes.update({
-					//id: i,
-					//group: group
-				//});
-				//group++;
-			}
-		}
-	}
+const update = function() {
+      var input = document.querySelector("#input").value;
+      input = input.split("\n");
+
+      for (var i = 0; i < input.length; i++) {
+            var split = input[i].split("\\");
+            var name = split[split.length - 1];
+            if (name.includes(".")) {
+                  group = 1;
+            } else {
+                  group = 2;
+            }
+            nodes.add({
+                  id: id,
+                  label: name,
+                  group: group,
+                  path: input[i]
+            });
+
+            id++;
+      }
+      for (var i = 0; i < input.length; i++) {
+            for (var j = 0; j < Object.keys(data.nodes._data).length; j++) {
+                  if (directory(input[i]) == data.nodes._data[j].path) {
+                        edges.add({
+                              from: j,
+                              to: i
+                        });
+                        //nodes.update({
+                        //id: i,
+                        //group: group
+                        //});
+                        //group++;
+                  }
+            }
+      }
 }
 
 var container = document.getElementById("network");
 
 var options = {
-	nodes: {
-		shape: 'dot',
-		size: 30,
-		font: {
-			size: 16
-		},
-		borderWidth: 2,
-		shadow:true
-	},
-	edges: {
-		width: 2,
-		shadow:true
-	}
+      nodes: {
+            shape: 'dot',
+            size: 30,
+            font: {
+                  size: 16
+            },
+            borderWidth: 2,
+            shadow: true
+      },
+      edges: {
+            width: 2,
+            shadow: true
+      }
 };
 network = new vis.Network(container, data, options);
