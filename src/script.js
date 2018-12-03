1d468f9da9df139667114bbd949f7e52e22ca9ee
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2018-12-03 16:13:12 -0500

    Add node connections to represent directory relationships

--- src/script.js
+++ src/script.js
@@ -3,6 +3,23 @@ var len = undefined;
 var nodes = new vis.DataSet();
 var edges = new vis.DataSet();
 
+var data = {
+	nodes: nodes,
+	edges: edges
+};
+
+const directory = function(input) {
+	var dir = "";
+	input = input.split("\\");
+	for (var i = 0; i < input.length - 1; i++) {
+		dir += input[i];
+		if (i < input.length - 2) {
+			dir += "\\";
+		}
+	}
+	return dir;
+}
+
 var id = 0;
 const update = function () {
 	var input = document.querySelector("#input").value;
@@ -14,18 +31,26 @@ const update = function () {
 		nodes.add({
 			id: id,
 			label: name,
-			group: 1
+			group: 1,
+			path: input[i]
 		});
 		
 		id++;
 	}
+	for (var i = 0; i < input.length; i++) {
+		for (var j = 0; j < Object.keys(data.nodes._data).length; j ++) {
+			if (directory(input[i]) == data.nodes._data[j].path) {
+				edges.add({
+					from: i,
+					to: j
+				});
+			}
+		}
+	}
 }
 
 var container = document.getElementById("network");
-var data = {
-	nodes: nodes,
-	edges: edges
-};
+
 var options = {
 	nodes: {
 		shape: 'dot',
